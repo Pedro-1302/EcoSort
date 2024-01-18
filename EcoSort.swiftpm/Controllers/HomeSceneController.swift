b1e1f639e2cf2c9b1d755a167c0e0f27bcc3b5af
Pedro Franco <mrcraftsoficcial@gmail.com>
2024-01-18 08:39:21 -0300

    Added player assets and recycle-symbol

---
+++ EcoSort.swiftpm/Controllers/HomeSceneController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeSceneController.swift
+//  EcoSort
+//
+//  Created by Pedro Franco on 18/01/24.
+//
+
+import Foundation
+import SpriteKit
+
+class HomeSceneController: SKScene {
+    override func sceneDidLoad() {
+        
+    }
+}
